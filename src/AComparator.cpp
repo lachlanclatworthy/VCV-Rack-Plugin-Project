44eca5fdb46e40df5a7f61e822d71f9db839e475
Lachlan Clatworthy <clatworthy.lachlan@gmail.com>
2022-08-26 13:01:22 +1000

    Update AComparator.cpp
    Add Documentation for AComparator and process()
    
    Add process() implementation

--- src/AComparator.cpp
+++ src/AComparator.cpp
@@ -2,6 +2,10 @@
 
 #include "plugin.hpp"		
 
+/// <summary>
+/// Takes two input voltages. Returns a high output voltage if Input 1 is higher than Input 2 -- 
+/// High Voltage = 10V, Low Voltage = 0V
+/// </summary>
 struct AComparator : Module {
 	enum ParamIds {
 		NUM_PARAMS,
@@ -31,5 +35,17 @@ struct AComparator : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 	}
 
+	/// <summary>
+	/// DSP Implementation. Operates sample by sample. Called periodically at sampling rate i.e. 44.1KHz
+	/// </summary>
+	/// <param name="args"></param>
 	void process(const ProcessArgs& args) override;
 };
+
+void AComparator::process(const ProcessArgs& args) {
+	if (inputs[INPUTA1].isConnected() && inputs[INPUTB1].isConnected()) {
+		float out = inputs[INPUTA1].getVoltage() >= input[INPUTB1].getVoltage();
+		outputs[OUTPUT1].setVoltage(10.f * out);
+		lights[OUTPUT1].setBrightness(out);
+	}
+}
