59a01f872a37bfd8cd676ea90f809b192695f2b1
Lachlan Clatworthy <clatworthy.lachlan@gmail.com>
2022-08-31 13:40:10 +1000

    Update AClock.cpp
    AClock Struct Declaration
    Enumerate params, inputs, outputs
    Add Constructor
    Declare process()

--- src/AClock.cpp
+++ src/AClock.cpp
@@ -3,7 +3,35 @@
 #include "plugin.hpp"		
 
 struct AClock : Module {
+    enum ParamIds {
+        BPM_KNOB,
+        NUM_PARAMS,
+    };
 
+    enum InputIds {
+        NUM_INPUTS,
+    };
+
+    enum OutputIds {
+        PULSE_OUT,
+        NUM_OUTPUTS,
+    };
+
+    enum LightIds {
+        PULSE_LIGHT,
+        NUM_LIGHTS,
+    };
+
+    dsp::PulseGenerator pgen;
+    float counter, period;
+
+    AClock() {
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        configParam(BPM_KNOB, 30.0f, 360.0f, 120.0f, "Tempo", "BPM");
+        counter = period = 0.f;
+    }
+
+    void process(const ProcessArgs& args) override; // called each sample
 };
 
 struct AClockWidget : ModuleWidget {
