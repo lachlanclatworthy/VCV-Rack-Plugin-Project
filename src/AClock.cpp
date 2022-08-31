be4e72bf36c5a6139feb88161bb49f325ad04f10
Lachlan Clatworthy <clatworthy.lachlan@gmail.com>
2022-08-31 16:40:03 +1000

    Implement AClock Module
    Simple AClock.svg GUI background created
    
    Shift BPM Unit String
    
    Implement AClock() process().
    
    Attempts to avoid jitter between system clock and desired clock by retaining a fractional portion of the diff between period and sample counter.
    
    Declare GUI Parameters in AClockWidget.

--- src/AClock.cpp
+++ src/AClock.cpp
@@ -1,6 +1,6 @@
 // AClock.cpp
 
-#include "plugin.hpp"		
+#include "plugin.hpp"
 
 struct AClock : Module {
     enum ParamIds {
@@ -27,19 +27,42 @@ struct AClock : Module {
 
     AClock() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        configParam(BPM_KNOB, 30.0f, 360.0f, 120.0f, "Tempo", "BPM");
+        configParam(BPM_KNOB, 30.0f, 360.0f, 120.0f, "Tempo", " BPM");
         counter = period = 0.f;
     }
 
-    void process(const ProcessArgs& args) override; // called each sample
-};
-
-struct AClockWidget : ModuleWidget {
-
+    void process(const ProcessArgs& args) override;  // called each sample
 };
 
 void AClock::process(const ProcessArgs& args) {
+    float BPM = params[BPM_KNOB].getValue();
+    period = 60.f * args.sampleRate / BPM;  // samples
 
+    if (counter >= period) {
+        pgen.trigger();
+        counter -= period;  // keep the fractional part
+    }
+
+    counter++;
+    float out = pgen.process(args.sampleTime);
+    outputs[PULSE_OUT].setVoltage(10.f * out);
+    lights[PULSE_LIGHT].setBrightnessSmooth(
+        out, 5e-6f);  // smooth light brightness to handle triggers between GUI
+                      // updates
 }
+
+struct AClockWidget : ModuleWidget {
+    AClockWidget(AClock* module) {
+        setModule(module);
+        setPanel(APP->window->loadSvg(
+            asset::plugin(pluginInstance, "res/AClock.svg")));
+
+        addParam(createParamCentered<RoundBigBlackKnob>(mm2px(Vec(32.5, 30)), module, AClock::BPM_KNOB));
+
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.5, 50)), module, AClock::PULSE_OUT));
+
+        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(22.5, 50)), module, AClock::PULSE_LIGHT));
+    }
+};
 
 Model* modelAClock = createModel<AClock, AClockWidget>("AClock");
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -10,3 +10,4 @@ extern Plugin* pluginInstance;
 // Declare each Model, defined in each module source file
 extern Model* modelAComparator;
 extern Model* modelAMuxDemux;
+extern Model* modelAClock;
