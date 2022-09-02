e74db9351d5d1f1d4ee6a36d21c9606d1a394871
Lachlan Clatworthy <clatworthy.lachlan@gmail.com>
2022-09-03 09:58:10 +1000

    Update ADivider.cpp

--- src/ADivider.cpp
+++ src/ADivider.cpp
@@ -3,7 +3,14 @@
 #include "plugin.hpp"
 
 struct div2 {
+    bool status;
 
+    div2() { status = false; }
+
+    bool process() {
+        status ^= 1;
+        return status;
+    }
 };
 
 struct ADivider : Module {
@@ -12,27 +19,39 @@ struct ADivider : Module {
     };
 
     enum InputIds {
+        MAIN_IN,
         NUM_INPUTS,
     };
 
     enum OutputIds {
+        OUTPUT1,
+        OUTPUT2,
+        OUTPUT4,
+        OUTPUT8,
+        OUTPUT16,
+        OUTPUT32,
         NUM_OUTPUTS,
     };
 
     enum LightIds {
+        LIGHT1,
+        LIGHT2,
+        LIGHT4,
+        LIGHT8,
+        LIGHT16,
+        LIGHT32,
         NUM_LIGHTS,
     };
 
-    ADivider() {
-        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-    }
+    div2 dividers[NUM_OUTPUTS];
+    dsp::PulseGenerator pgen[NUM_OUTPUTS];
+
+    ADivider() { config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS); }
 
     void process(const ProcessArgs& args) override;
 };
 
-void ADivider::process(const ProcessArgs& args) {
-
-}
+void ADivider::process(const ProcessArgs& args) {}
 
 struct ADividerWidget : ModuleWidget {
     ADividerWidget(ADivider* module) {
@@ -45,9 +64,8 @@ struct ADividerWidget : ModuleWidget {
         // Output
 
         // Parameters
-        
-        // Lights
 
+        // Lights
     }
 };
 
