433492f42d4d43a5efe5f90ba3e779445f1e9463
Lachlan Clatworthy <clatworthy.lachlan@gmail.com>
2022-09-03 10:53:50 +1000

    Update ADivider.cpp

--- src/ADivider.cpp
+++ src/ADivider.cpp
@@ -2,6 +2,8 @@
 
 #include "plugin.hpp"
 
+const float TRIG_TIME = 0.5f;
+
 struct div2 {
     bool status;
 
@@ -46,12 +48,40 @@ struct ADivider : Module {
     div2 dividers[NUM_OUTPUTS];
     dsp::PulseGenerator pgen[NUM_OUTPUTS];
 
+    /// @brief Iteratively divides clock pulse by 2 for each output
+    /// @param idx
+    void iterActiv(int idx) {
+        if (idx > NUM_OUTPUTS - 1) return;  // stop iteration
+
+        bool activation = dividers[idx].process();
+
+        pgen[idx].trigger(TRIG_TIME);
+
+        if (activation) {
+            iterActiv(idx + 1);
+        }
+    }
+
+    dsp::SchmittTrigger
+        edgeDetector;  // activates on a clock edge, stays high until it reaches
+                       // a low threshold (i.e. 0)
+
     ADivider() { config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS); }
 
     void process(const ProcessArgs& args) override;
 };
 
-void ADivider::process(const ProcessArgs& args) {}
+void ADivider::process(const ProcessArgs& args) {
+    if (edgeDetector.process(inputs[MAIN_IN].getVoltage())) {
+        iterActiv(0);  // run the first divider (/2) and iterate if necessary
+    }
+
+    for (int i = 0; i < NUM_OUTPUTS; i++) {
+        float out = pgen[i].process(args.sampleTime);
+        outputs[i].setVoltage(10.f * out);
+        lights[i].setBrightnessSmooth(out, 0.5f);
+    }
+}
 
 struct ADividerWidget : ModuleWidget {
     ADividerWidget(ADivider* module) {
